fd4e2129642d509682d0082016b3b5a5784ddabc
Adrián Maza <adri_mv1@hotmail.com>
2024-06-26 16:43:12 +0200

    w3d3

--- src/main/java/com/ironhack/spring_lessons/SpringLessonsApplication.java
+++ src/main/java/com/ironhack/spring_lessons/SpringLessonsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringLessonsApplication {
 
+//	Week 3 day 3
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLessonsApplication.class, args);
 	}
