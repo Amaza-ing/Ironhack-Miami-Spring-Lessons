5286c575dde6da75e97cbe72afabd05da3d67c22
Adrián Maza <adri_mv1@hotmail.com>
2024-07-01 16:48:05 +0200

    w4d1 controllers GET request

--- src/main/java/com/ironhack/spring_lessons/SpringLessonsApplication.java
+++ src/main/java/com/ironhack/spring_lessons/SpringLessonsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringLessonsApplication {
 
+//	Week 4 day 1 - Controllers - GET request
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLessonsApplication.class, args);
 	}
