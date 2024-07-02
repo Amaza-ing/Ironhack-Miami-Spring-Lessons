c88ef24160b4ed345d774ec7f4280b7ae96e7d80
Adrián Maza <adri_mv1@hotmail.com>
2024-07-02 16:42:55 +0200

    Sevices & POST, PUT, PATCH, DELETE Requests)

--- src/main/java/com/ironhack/spring_lessons/controller/impl/CourseController.java
+++ src/main/java/com/ironhack/spring_lessons/controller/impl/CourseController.java
@@ -1,8 +1,13 @@
 package com.ironhack.spring_lessons.controller.impl;
 
+import com.ironhack.spring_lessons.controller.dto.CourseClassroomDTO;
+import com.ironhack.spring_lessons.controller.dto.CourseHoursDTO;
 import com.ironhack.spring_lessons.model.Course;
 import com.ironhack.spring_lessons.repository.CourseRepository;
+import com.ironhack.spring_lessons.service.interfaces.ICourseService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,23 +17,24 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class CourseController {
     @Autowired
-    CourseRepository courseRepository;
+    ICourseService courseService;
+
+
+    //  ****************************************************  GET  ****************************************************
 
     @GetMapping("/courses")
     public List<Course> getAllCourses() {
-        return courseRepository.findAll();
+        return courseService.getAllCourses();
     }
 
     @GetMapping("/courses/{course}")
     public Course getCourseById(@PathVariable String course) {
-        Optional<Course> courseOptional = courseRepository.findById(course);
-        if (courseOptional.isEmpty()) return null;
-        return courseOptional.get();
+        return courseService.getCourseById(course);
     }
 
     @GetMapping("/courses/hours")
     public List<Course> getCoursesByHoursLessThan(@RequestParam(defaultValue = "100") Integer hours) {
-        return courseRepository.findAllByHoursLessThan(hours);
+        return courseService.getCoursesByHoursLessThan(hours);
     }
 
     @GetMapping("/courses/classroom")
@@ -36,7 +42,50 @@ public class CourseController {
             @RequestParam(defaultValue = "A1") String classroom,
             @RequestParam Optional<Integer> hours
     ) {
-        if (hours.isPresent()) return courseRepository.findAllWhereClassroomAndHoursParams(classroom, hours.get());
-        return courseRepository.findAllByClassroom(classroom);
+        return courseService.getCoursesByClassroomAndHours(classroom, hours);
     }
+
+
+    //  ***************************************************  POST  ****************************************************
+
+    @PostMapping("/courses")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveCourse(@RequestBody @Valid Course course) {
+        courseService.saveCourse(course);
+    }
+
+
+    //  ****************************************************  PUT  ****************************************************
+
+    @PutMapping("/courses/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCourse(@RequestBody @Valid Course course, @PathVariable String id) {
+        courseService.updateCourse(course, id);
+    }
+
+
+    //  ***************************************************  PATCH  ***************************************************
+
+    @PatchMapping("/courses/hours/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCourseHours(@RequestBody @Valid CourseHoursDTO courseHoursDTO, @PathVariable String id) {
+        courseService.updateCourseHours(courseHoursDTO.getHours(), id);
+    }
+
+    @PatchMapping("/courses/classroom/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCourseClassroom(@RequestBody @Valid CourseClassroomDTO courseClassroomDTO, @PathVariable String id) {
+        courseService.updateCourseClassroom(courseClassroomDTO.getClassroom(), id);
+    }
+
+
+    //  **************************************************  DELETE  ***************************************************
+
+    @DeleteMapping("/courses/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCourse(@PathVariable String id) {
+        courseService.deleteCourse(id);
+    }
+
+
 }
